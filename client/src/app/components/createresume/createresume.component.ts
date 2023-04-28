78ca0dc33653d169af5537db073b73834f012b5d
Home <none@gmail.com>
2023-04-28 09:47:06 +0300

    Date_to_CreateResume

--- client/src/app/components/createresume/createresume.component.ts
+++ client/src/app/components/createresume/createresume.component.ts
@@ -1,14 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgbDate, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-createresume',
   templateUrl: './createresume.component.html',
   styleUrls: ['./createresume.component.css']
 })
-export class CreateresumeComponent
-    implements OnInit {
+export class CreateresumeComponent implements OnInit {
+
+    fromDate: NgbDate;
+    toDate: NgbDate;
+    hoveredDate: NgbDate;
+    closeResult: string;
+    model1: NgbDate;
+    model2: NgbDate;
+    model: NgbDate;
     focus;
-  constructor() { }
+    focus1;
+    focus2;
+    focus3;
+    focus4;
+    constructor(private modalService: NgbModal, calendar: NgbCalendar) {
+        this.fromDate = calendar.getToday();
+        this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
+    }
 
   ngOnInit(): void {
   }
