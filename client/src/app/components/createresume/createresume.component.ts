ea39b06e86d585941a047dc8c8c4e018513d3026
Home <none@gmail.com>
2023-04-29 10:14:28 +0300

    Resume_Create_Step_2

--- client/src/app/components/createresume/createresume.component.ts
+++ client/src/app/components/createresume/createresume.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbDate, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
-  selector: 'app-createresume',
-  templateUrl: './createresume.component.html',
-  styleUrls: ['./createresume.component.css']
+    selector: 'app-createresume',
+    templateUrl: './createresume.component.html',
+    styleUrls: ['./createresume.component.css']
 })
+
 export class CreateresumeComponent implements OnInit {
 
     fromDate: NgbDate;
@@ -20,12 +22,18 @@ export class CreateresumeComponent implements OnInit {
     focus2;
     focus3;
     focus4;
+
+    page = 2;
+    page1 = 3;
+    active = 1;
+    active1 = 1;
+    active2 = 1;
+
     constructor(private modalService: NgbModal, calendar: NgbCalendar) {
         this.fromDate = calendar.getToday();
         this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
     }
 
-  ngOnInit(): void {
-  }
+    ngOnInit() { }
 
 }
--- client/src/app/components/resumes/resumes.component.ts
+++ client/src/app/components/resumes/resumes.component.ts
@@ -1,18 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 @Component({
-  selector: 'app-resumes',
-  templateUrl: './resumes.component.html',
-  styleUrls: ['./resumes.component.css']
+    selector: 'app-resumes',
+    templateUrl: './resumes.component.html',
+    styleUrls: ['./resumes.component.css']
 })
 export class ResumesComponent implements OnInit {
 
     constructor(private router: Router) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void { }
+
     navigateCreateResume() {
         this.router.navigate(['/resume/create']);
-        console.log('HELLO');
-}
+    }
 }
